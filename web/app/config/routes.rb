3870a17cb7ed836ee5164b4811600fcf42e155b8
Andres <andreskal@gmail.com>
2015-11-20 14:47:20 +0000

    add redirect root

--- web/app/config/routes.rb
+++ web/app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "breeds#index"
   resources :countries
   resources :breeds
   # The priority is based upon order of creation: first created -> highest priority.
