2038024668fb49fd49a3310765c8423632d7849b
Andres Colonia <andreskal@gmail.com>
2018-05-15 13:51:56 -0400

    some fixes

--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -21,11 +21,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = Rails.env.production?
+  config.public_file_server.enabled = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
